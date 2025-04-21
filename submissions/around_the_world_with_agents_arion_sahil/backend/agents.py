1ec24e31d2357b52cb8551cc05dd8999e68270d8
Arion Das <ariondasad@gmail.com>
2025-04-21 19:31:58 +0530

    project_launch

---
+++ submissions/around_the_world_with_agents_arion_sahil/backend/agents.py
@@ -0,0 +1,15 @@
+from textwrap import dedent
+
+from agno.agent import Agent
+from agno.models.anthropic import Claude
+from agno.models.openai import OpenAIChat
+from agno.team.team import Team
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.reasoning import ReasoningTools
+from agno.tools.yfinance import YFinanceTools
+
+
+## Transport Agent
+transport_agent = Agent(
+    
+)
